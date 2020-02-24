0284c9c71c92d7a23032ea5017fa21f834dfa992
Artur <Artur.Radzivilov@stud.vgtu.lt>
2020-02-24 21:37:41 +0200

    Changed village and added coin count table

--- Assets/Scripts/Collections/Coin.cs
+++ Assets/Scripts/Collections/Coin.cs
@@ -16,4 +16,7 @@ public class Coin : MonoBehaviour
             Count++;
         }
     }
+
+    static public int GetCoinsCount() => Count;
+
 }
---
+++ Assets/Scripts/UI/CoinsUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsUI : MonoBehaviour
+{
+
+    /// <summary>
+    /// Contains max 9 digits number
+    /// </summary>
+    private Text CoinsText;
+
+    private const string CoinsStr = " Coins: "; 
+
+    void Start()
+    {
+        CoinsText = this.GetComponent<Text>();
+        CoinsText.text = CoinsStr + Coin.GetCoinsCount().ToString();
+    }
+
+    void Update()
+    {
+        CoinsText.text = CoinsStr + Coin.GetCoinsCount().ToString();
+    }
+}
